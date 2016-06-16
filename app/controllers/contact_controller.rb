ae7d90304b03e445eab104a54f585fb11d267da4
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-06-16 00:53:13 -0700

    add info company

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,7 @@
 class ContactController < ApplicationController
     def index
         @contact = Contact.new
+        @company = Company.get_company
     end
     def message_sent_success
     end
