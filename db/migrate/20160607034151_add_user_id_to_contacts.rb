3e6d727a86dbf4c9784eb82344c4725f878965dd
Luan Pham <luanpm88@gmail.com>
2016-06-21 09:32:43 +0700

    server

--- db/migrate/20160607034151_add_user_id_to_contacts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToContacts < ActiveRecord::Migration
-  def change
-    add_column :contacts, :user_id, :integer
-  end
-end
--- db/migrate/20160607034155_add_user_id_to_newsletters.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToNewsletters < ActiveRecord::Migration
-  def change
-    add_column :newsletters, :user_id, :integer
-  end
-end
