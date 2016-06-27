7bc0b10d4423cc81fe16e20eb961832928b6a00b
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-06-26 18:29:06 -0700

    fix phan tram giam gia --> làm tròn lên 1 chu so thap phan + translate module Project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616073753) do
+ActiveRecord::Schema.define(version: 20160621025518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -376,6 +376,7 @@ ActiveRecord::Schema.define(version: 20160616073753) do
     t.integer  "stock",             default: 1
     t.boolean  "approved",          default: false
     t.integer  "user_id"
+    t.boolean  "fb_shared",         default: false
   end
 
   create_table "program_languages", force: :cascade do |t|
