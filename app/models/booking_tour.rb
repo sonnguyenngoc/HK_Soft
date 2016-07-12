6ab065ccf43798b37a33bb516b33210c072512da
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-07-12 01:07:09 -0700

    update

--- app/models/booking_tour.rb
+++ app/models/booking_tour.rb
@@ -2,6 +2,8 @@ class BookingTour < ActiveRecord::Base
     belongs_to :tour
     belongs_to :tour_schedule
     before_validation :update_tour_info
+    validates :full_name, :address, :phone, :email, :tour_schedule_id, :child, :child_2, :child_3, :adult, :message, presence: true
+    validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
     
     def update_tour_info
         self.tour_name = tour.name
