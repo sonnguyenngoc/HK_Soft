5700da1df7fa69666fc5ef2a8b80185ad5419e27
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-16 11:40:26 -0700

    contact infomations in footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
     def index
         @new_products = Product.get_by_new
         @lastest_blogs = Article.get_lastest_blog_posts
+        @info_contact = Company.all
     end
 end
