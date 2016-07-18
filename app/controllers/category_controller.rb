044680125020df7d7754902167a02dd676faa098
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-17 21:29:34 -0700

    update pagination

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,18 +2,18 @@ class CategoryController < ApplicationController
   def index
     @info_contact = Company.all
     @category = Category.find(params[:category_id])
-    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => params[:number])
+    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => 20)
   end
   
   def category_sub_1
     @info_contact = Company.all
     @category = Category.find(params[:category_id])
-    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => params[:number])
+    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => 20)
   end
   
   def category_sub_2
     @info_contact = Company.all
     @category = Category.find(params[:category_id])
-    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => params[:number])
+    @products = @category.get_products_for_categories(params).paginate(:page => params[:page], :per_page => 20)
   end
 end
