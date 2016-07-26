5542ebb49569106c21da84afebcaeb82076b4192
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-26 00:51:19 -0700

    cap nhat user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 # User.destroy_all
 #.....
-User.create(:email => "admin@hoangkhang.com.vn", :password => "aA456321@", :password_confirmation => "aA456321@", first_name: "Supper", last_name: "Admin", phone: "01644046123", address_1: "188 Street 1, Ward 16, Go Vap Dist,", is_admin: true, user_group_id: 3)
+User.create(:email => "admin@lumlabeauty.com", :password => "aA456321@", :password_confirmation => "aA456321@", first_name: "Supper", last_name: "Admin", phone: "0126 216 9420", address_1: "188 Street 1, Ward 16, Go Vap Dist,", is_admin: true, user_group_id: 3)
 #.....
 
 UserGroup.create(name: "Thành viên")
 UserGroup.create(name: "Quản trị viên")
-UserGroup.create(name: "Quản lý cấp cao", permission: "{\"User\":{\"read\":[\"yes\"],\"create\":[\"yes\"],\"update\":[\"yes\"],\"delete\":[\"yes\"]},\"UserGroup\":{\"read\":[\"yes\"],\"create\":[\"yes\"],\"update\":[\"yes\"],\"delete\":[\"yes\"]}}")
+UserGroup.create(name: "Quản lý cấp cao")
