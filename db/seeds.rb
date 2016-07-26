29723942c2cb9f3fe638b29c8c3e7c55426c2968
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-25 23:37:19 -0700

    set permission for quan-ly-cap-cao

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 # User.destroy_all
 #.....
-User.create(:email => "admin@hoangkhang.com.vn", :password => "aA456321@", :password_confirmation => "aA456321@", first_name: "Quản trị", last_name: "viên", phone: "01644046123", address_1: "54/3, Tăng NHơn Phú B, Quận 9", is_admin: true)
+User.create(:email => "admin@hoangkhang.com.vn", :password => "aA456321@", :password_confirmation => "aA456321@", first_name: "Supper", last_name: "Admin", phone: "01644046123", address_1: "188 Street 1, Ward 16, Go Vap Dist,", is_admin: true, user_group_id: 3)
 #.....
 
 UserGroup.create(name: "Thành viên")
 UserGroup.create(name: "Quản trị viên")
-UserGroup.create(name: "Quản lý cấp cao")
+UserGroup.create(name: "Quản lý cấp cao", permission: "{\"User\":{\"read\":[\"yes\"],\"create\":[\"yes\"],\"update\":[\"yes\"],\"delete\":[\"yes\"]},\"UserGroup\":{\"read\":[\"yes\"],\"create\":[\"yes\"],\"update\":[\"yes\"],\"delete\":[\"yes\"]}}")
