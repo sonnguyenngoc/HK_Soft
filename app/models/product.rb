f6b81fcfdb6fc7051d1dceaef2ef658a4c345bb6
Luan Pham <luanpm88@gmail.com>
2016-08-12 18:54:29 -0700

    update facebook auto restart share

--- app/models/product.rb
+++ app/models/product.rb
@@ -391,6 +391,8 @@ class Product < ActiveRecord::Base
   end
   
   def self.share_facebook
+    Product.where(fb_shared: true).update_all(fb_shared: false) if Product.where(fb_shared: false).empty?
+    
     last_shared = Product.where(fb_shared: true).order("products.updated_at DESC").first
     last_shared = last_shared.nil? ? Time.now - 1.year : last_shared.updated_at
     if last_shared < (Time.now - 3.hours)
@@ -403,6 +405,7 @@ class Product < ActiveRecord::Base
     if !@share_item.nil?
       # @user = Koala::Facebook::API.new('EAACEdEose0cBAErceaWRPnQDzdITApzR5wF1pY13FnRaz5MOq69UnFznwIfZAm1x3RNZAzlckMaTDPDHJvxhGO0tx1gkBs1vD7HAQeZBFGR2cz2mHKs0ZBaFIw13e4FURTmuIgBKSDLxU5P0BsObyBZBBlZCWxTbLcpZCicQzYIywZDZD')
       @user = Koala::Facebook::API.new('EAAIl2ILpcl0BADLPsfZAXu0CqPttEiD0HDduCXjRW7GPUnlENCmckhGmZCbLTlhfj5y6Fa1iycbMW9PRvS63TZBKtyWLRRWi8sYFL14I5PlsNOMEvlhDGCxZCUdgPF2mvoAfRla8avjIoY8s4JSqFki9CKKjoxwZD')
+      
       @article = Article.get_facebook_share_message
       if !@article.nil?
         #begin
